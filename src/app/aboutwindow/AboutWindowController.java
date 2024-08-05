941439ef5b81c0a5a0dcfa542190842b5d43c4a2
JovanMosurovic <mosurovicjovan@gmail.com>
2024-08-05 17:30:52 +0200

    Improved UI for about window.

--- src/app/aboutwindow/AboutWindowController.java
+++ src/app/aboutwindow/AboutWindowController.java
@@ -4,14 +4,15 @@ import app.ControllerBase;
 import app.Window;
 import app.WindowHelper;
 import javafx.application.HostServices;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Hyperlink;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AboutWindowController extends ControllerBase {
     private HostServices hostServices;
+    private static final String PROJECT_GITHUB_URL = "https://github.com/JovanMosurovic/Elemental_SQL_Implementation_with_GUI";
+    private static final String AUTHOR_GITHUB_URL = "https://github.com/JovanMosurovic";
+    private static final String INSTRUCTIONS_GITHUB_URL = "https://github.com/JovanMosurovic/Elemental_SQL_Implementation_with_GUI/blob/master/instructions.pdf";
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Window.getWindowAt(Window.ABOUT_WINDOW).setController(this);
@@ -21,10 +22,20 @@ public class AboutWindowController extends ControllerBase {
         WindowHelper.hideWindow(Window.ABOUT_WINDOW);
     }
 
-    public void handleOpenLink(ActionEvent event) {
-        Hyperlink source = (Hyperlink) event.getSource();
-        String url = source.getText();
-        if(hostServices != null) {
+    public void handleOpenProjectGitHub() {
+        openLink(PROJECT_GITHUB_URL);
+    }
+
+    public void handleOpenAuthorGitHub() {
+        openLink(AUTHOR_GITHUB_URL);
+    }
+
+    public void handleOpenInstructionsLink() {
+        openLink(INSTRUCTIONS_GITHUB_URL);
+    }
+
+    private void openLink(String url) {
+        if (hostServices != null) {
             hostServices.showDocument(url);
         }
     }
