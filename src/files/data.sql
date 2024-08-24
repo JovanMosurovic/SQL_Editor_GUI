9c2fdd8806ed122bd913e3dab4075e798c0237ec
JovanMosurovic <mosurovicjovan@gmail.com>
2024-08-24 19:42:56 +0200

    Added test files.

---
+++ src/files/data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE prvaTabela (arg, arg2, arg3, arg4);
+INSERT INTO prvaTabela (arg, arg2, arg3, arg4) VALUES ("test", "test1", "test2", "test3");
+INSERT INTO prvaTabela (arg, arg2, arg3, arg4) VALUES ("test11", "test111", "test211", "test311");
+INSERT INTO prvaTabela (arg, arg2, arg3, arg4) VALUES ("test555", "test1555", "test2555", "test3555");
+INSERT INTO prvaTabela (arg, arg2, arg3, arg4) VALUES ("test555", "test1555", "test2555", "test3555");
+CREATE TABLE drugaTabela (arg, nekiValue, test);
+INSERT INTO drugaTabela (arg, nekiValue, test) VALUES ("test", "value1", "value2");
+INSERT INTO drugaTabela (arg, nekiValue, test) VALUES ("test11", "value3", "value4");
+INSERT INTO drugaTabela (arg, nekiValue, test) VALUES ("test11", "value5", "value6");
